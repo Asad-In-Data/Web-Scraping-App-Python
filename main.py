76e40ca11b9a1bd0c4c9b6364ebf6909d58af4a4
Asad Ali <141873003+Asad-In-Data@users.noreply.github.com>
2025-08-26 17:34:18 +0500

    Scrape data from website and make a simple Ui

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from scraper import scrape_website
 
 st.title("WEB SCRAPER")
 url = st.text_input("Enter Website URL to scrape:")
@@ -6,6 +7,8 @@ url = st.text_input("Enter Website URL to scrape:")
 #     st.write(f"Scraping {url}...")
 if st.button("Start Scraping"):
         st.write("Scraping in progress...")
+        result= scrape_website(url)
+        print(result)
         # Placeholder for scraping logic
         st.write("Scraping completed!")
         st.success("Data scraped successfully!")
--- scraper.py
+++ scraper.py
@@ -1,17 +1,24 @@
-# import selenium.webdriver as webdriver
-# from selenium.webdriver.chrome.service import Service
-
-from selenium import webdriver
+import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-# Chrome driver auto install aur launch
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+import time
 
-# Website open karo
-driver.get("https://www.google.com")
+def scrape_website(url):
+    # Chrome driver auto install aur launch
+ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-print("Page Title:", driver.title)
-
-# Browser band karo
-driver.quit()
+    # Website open karo
+ try: 
+       driver.get(url)
+       print("Page Title and loading........", driver.title)
+       html_content = driver.page_source
+       time.sleep(20)
+       
+       return html_content
+ except Exception as e:
+       print("Error occurred:", e)
+ finally:
+       # Browser band karo
+       driver.quit()
+       

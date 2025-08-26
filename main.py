4b58a6384a5dd2cc0cc81e49c3d7a49a14063d3e
Asad Ali <141873003+Asad-In-Data@users.noreply.github.com>
2025-08-27 01:12:56 +0500

    added functions to Unblock Websites
     Random User-Agent rotation , Random human-like pauses Free and reliable proxy rotation

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ st.title("WEB SCRAPER")
 url = st.text_input("Enter Website URL to scrape:")
 # if url:
 #     st.write(f"Scraping {url}...")
+
 if st.button("Start Scraping"):
         st.write("Scraping in progress...")
         result= scrape_website(url)
-        print(result)
-        # Placeholder for scraping logic
-        st.write("Scraping completed!")
-        st.success("Data scraped successfully!")
+        if not result:
+          st.error("Scraping failed! Possibly blocked or invalid URL.")
+        else:
+          st.success("Scraping successful!")
+          st.success("Data scraped successfully!")
--- scraper.py
+++ scraper.py
@@ -1,20 +1,20 @@
 import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
+from selenium.webdriver.chrome.options import Options
+import random
 import time
 
 def scrape_website(url):
     # Chrome driver auto install aur launch
- driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
+#  driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+ driver = make_driver()
     # Website open karo
  try: 
        driver.get(url)
        print("Page Title and loading........", driver.title)
        html_content = driver.page_source
-       time.sleep(20)
-       
+       human_pause(2, 5)
        return html_content
  except Exception as e:
        print("Error occurred:", e)
@@ -22,3 +22,28 @@ def scrape_website(url):
        # Browser band karo
        driver.quit()
        
+
+def human_pause(a=2, b=5):
+    """Wait like a human (random between a and b seconds)."""
+    t = random.uniform(a, b)
+    print(f"[pause] waiting {t:.2f} seconds...")
+    time.sleep(t)      
+
+
+
+
+UA_POOL = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.3 Safari/605.1.15",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+]
+
+def make_driver():
+    ua = random.choice(UA_POOL)   # pick a random UA
+    print(f"[info] using UA: {ua}")
+
+    opts = Options()
+    opts.add_argument(f"--user-agent={ua}")
+    opts.add_argument("--disable-blink-features=AutomationControlled")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=opts)
+    return driver
